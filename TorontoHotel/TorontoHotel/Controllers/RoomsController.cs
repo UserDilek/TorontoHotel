65ca5a0bf6a17914199df9e630ef50391c01ed82
ozgurkara <Dhocaoglukara9037@conestogac.on.ca>
2024-02-07 17:20:45 -0500

    create Root Contoller

---
+++ TorontoHotel/TorontoHotel/Controllers/RoomsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TorontoHotel.Controllers
+{
+	[Route("/[controller]")]
+	[ApiController]
+	public class RoomsController:ControllerBase
+	{
+		[HttpGet(Name =nameof(GetRooms))]
+		public IActionResult GetRooms()
+		{
+			throw new Exception();
+		}
+	}
+}
+
---
+++ TorontoHotel/TorontoHotel/Controllers/RootController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TorontoHotel.Controllers
+{
+	[Route("/")]
+	[ApiController]
+	public class RootController : Controller
+	{
+		[HttpGet(Name =nameof(GetRoot))]
+		public IActionResult GetRoot()
+		{
+			var response = new
+			{
+				href = Url.Link(nameof(GetRoot),null),
+				room = new
+				{
+                    href = Url.Link(nameof(RoomsController.GetRooms), null),
+                }
+				
+		   }; 
+			return Ok(response);
+
+        }
+	}
+}
+
